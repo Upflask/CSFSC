ece20226ef3806f71b0173f30c68db5b333f574e
Bitstick <thebitstick@tfwno.gf>
2016-02-25 10:12:27 -0600

    Removed unnecessary "if" statement

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ $(document).ready(function() {
      var iSize = ($("#file")[0].files[0].size / 1024); 
      if (iSize * 1024 * 1024 > 40 * 1024 * 1024) 
      { 
-        if ((iSize))
-        { 
-           iSize = (Math.round(iSize))
-           alert(('This file is over the 40MB limit, try another one.'));
-           location.reload();
-        }
+        iSize = (Math.round(iSize))
+        alert(('This file is over the 40MB limit, try another one.'));
+        location.reload();
      } 
      else 
      {
