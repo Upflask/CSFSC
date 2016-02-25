15e35488199326c4d6c134b547594470739408f1
Bitstick <thebitstick@tfwno.gf>
2016-02-25 10:05:24 -0600

    First try

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+   $("#file").change(function () 
+   { 
+     var iSize = ($("#file")[0].files[0].size / 1024); 
+     if (iSize * 1024 * 1024 > 40 * 1024 * 1024) 
+     { 
+        if ((iSize))
+        { 
+           iSize = (Math.round(iSize))
+           alert(('This file is over the 40MB limit, try another one.'));
+           location.reload();
+        }
+     } 
+     else 
+     {
+        iSize = (Math.round(iSize))
+     }    
+  }); 
+});
